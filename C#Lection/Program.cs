c64704da71f4e30d8b28b986b712cbff46976415
AK8k <kuklin_aleksey@mail.ru>
2022-09-01 22:46:29 +0800

    Maria

--- C#Lection/Program.cs
+++ C#Lection/Program.cs
@@ -5,11 +5,25 @@ Console.WriteLine(numberA / numberB);
 */
 
 
-
+/*
 int numberA = new Random().Next(1, 10);
 int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
 Console.WriteLine(numberA);
 Console.WriteLine(numberB);
 Console.WriteLine(result);
+*/
 
+
+
+Console.WriteLine("Enter user name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Hi Maria! we find u");
+
+}
+else
+{
+    Console.WriteLine($"Hi, {username} ");
+}
