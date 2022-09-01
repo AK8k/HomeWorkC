caa0e5fa515a99bc73bc6252eb9edeea74b8f284
AK8k <kuklin_aleksey@mail.ru>
2022-09-01 22:57:30 +0800

    GIRI

--- C#Lection/Program.cs
+++ C#Lection/Program.cs
@@ -15,7 +15,7 @@ Console.WriteLine(result);
 */
 
 
-
+/*
 Console.WriteLine("Enter user name: ");
 string username = Console.ReadLine();
 if (username.ToLower() == "maria")
@@ -27,3 +27,24 @@ else
 {
     Console.WriteLine($"Hi, {username} ");
 }
+
+*/
+
+
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine("max =");
+Console.WriteLine(max);
+
+
