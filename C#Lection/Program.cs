21e6a06734e35d07145bb922ce26f2c88af5b466
AK8k <kuklin_aleksey@mail.ru>
2022-09-01 20:33:35 +0800

    Start Lection

---
+++ C#Lection/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+double numberA = 1;
+double numberB = 3;
+Console.WriteLine(numberA / numberB);
+*/
+
+
+
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(result);
+
